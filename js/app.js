e9aed4be57d46b73473a0c42ed94d9c407857ef9
StephanieS <stranosteph@gmail.com>
2023-11-13 15:38:10 -0500

    Set up project for Vue

--- js/app.js
+++ js/app.js
@@ -1,53 +1,48 @@
 const todoList = document.getElementById('todos');
+
 let todos = [];
 let pageTitle = '';
 
-// init() will set up the page title and tasks
 function init() {
-    pageTitle = 'My Morning Routine';
-    todos = [
-      { id: 1, task: 'Wake up', completed: false },
-      { id: 2, task: 'Brush Teeth', completed: false },
-      { id: 3, task: 'Shower', completed: false },
-      { id: 4, task: 'Get Dressed', completed: false },
-      { id: 5, task: 'Drive to work', completed: false },
-      { id: 6, task: 'Work', completed: false },
-      { id: 7, task: 'Drive home from work', completed: false },
-      { id: 8, task: 'Dinner', completed: false },
-      { id: 9, task: 'Brush Teeth', completed: false },
-      { id: 10, task: 'Go to bed', completed: false }
-    ]
-  }
+  pageTitle = 'My Morning Routine';
+  todos = [
+    { id: 1, task: 'Wake up', completed: false },
+    { id: 2, task: 'Brush Teeth', completed: false },
+    { id: 3, task: 'Shower', completed: false },
+    { id: 4, task: 'Get Dressed', completed: false },
+    { id: 5, task: 'Drive to work', completed: false },
+    { id: 6, task: 'Work', completed: false },
+    { id: 7, task: 'Drive home from work', completed: false },
+    { id: 8, task: 'Dinner', completed: false },
+    { id: 9, task: 'Brush Teeth', completed: false },
+    { id: 10, task: 'Go to bed', completed: false }
+  ];
+}
 
+function addPageTitle() {
+  const heading = document.createElement('h1');
+  heading.innerText = pageTitle;
+  todoList.appendChild(heading);
+}
 
-  // addPageTitle() will add the page title to the DOM
-  function addPageTitle() {
-    const heading = document.createElement('h1')
-    heading.innerText = pageTitle;
-    todoList.appendChild(heading);
-  }
-
-  // addTodos() will add the task to the DOM
-  function addTodos() {
-    const ul = document.createElement('ul');
-
-    todos.forEach(todo => {
-      const li = document.createElement('li');
-      li.innerText = todo.task;
-      const checkCircle = document.createElement('i');
-      checkCircle.setAttribute('class', 'far fa-check-circle');
-      li.appendChild(checkCircle);
-      ul.appendChild(li);
-    });
-    todoList.appendChild(ul);
-  }
-
-
-  document.addEventListener("DOMContentLoaded", () => {
-    init();
-    addPageTitle();
-    addTodos();
+function addTodos() {
+  const ul = document.createElement('ul');
+  todos.forEach((todo) => {
+    const li = document.createElement('li');
+    li.innerText = todo.task;
+    const checkCircle = document.createElement('i');
+    checkCircle.setAttribute('class', 'far fa-check-circle');
+    li.appendChild(checkCircle);
+    ul.appendChild(li);
   });
+  todoList.appendChild(ul);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  init();
+  addPageTitle();
+  addTodos();
+});
 
 // Marking tasks incomplete / complete
 const tasks = document.querySelectorAll('li');
